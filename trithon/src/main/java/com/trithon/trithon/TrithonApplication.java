37176eabe41db0608f3ce67b56ebe3b008436575
SangYeong Park <tkddud386@gmail.com>
2025-03-29 17:04:07 +0900

    initial commit

---
+++ trithon/src/main/java/com/trithon/trithon/TrithonApplication.java
@@ -0,0 +1,13 @@
+package com.trithon.trithon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrithonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrithonApplication.class, args);
+	}
+
+}
